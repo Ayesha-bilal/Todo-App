8b33fc02c78f226c310594c26715b679ce4c8c5a
Ayesha-bilal <ayeshaqureshi33333@gmail.com>
2024-07-12 04:21:40 +0500

    todo app

---
+++ index.ts
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+
+import chalk from "chalk";
+
+let todoList :string[] = []
+
+let condition= true
+
+console.log(chalk.bold.blue("\n \tWelcome to my Todo Application\n"));
+
+while(condition){
+  let addTask = await inquirer.prompt([
+    {
+     name:"task",
+     type:"input",
+     message:chalk.blueBright("Enter your new task")
+    }
+  ]);
+   todoList.push(addTask.task)
+   console.log(`${addTask.task} task added in Todo-List Successfully`);
+   
+   let addMoreTask=await inquirer.prompt([
+    {
+        name:"anotherTask",
+        type:"confirm",
+        message:chalk.blueBright("Do you want to add more task ?"),
+        default:false
+    }
+   ]);
+   condition= addMoreTask.anotherTask
+}
+
+console.log("Your updated Todo list:" , todoList);
